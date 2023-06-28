d86df1965deccc0f316b667beee51fa8ec3fa610
drondon87 <domingorondon87@gmail.com>
2023-06-28 16:15:32 +0200

    Seccion 9 Resilence4j

--- accounts/src/main/java/com/msvc/bank/accounts/controller/AccoutsController.java
+++ accounts/src/main/java/com/msvc/bank/accounts/controller/AccoutsController.java
@@ -22,6 +22,10 @@ import com.msvc.bank.accounts.repository.AccountsRepository;
 import com.msvc.bank.accounts.service.client.CardsFeignClient;
 import com.msvc.bank.accounts.service.client.LoansFeignClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class AccoutsController {
 
@@ -58,6 +62,8 @@ public class AccoutsController {
 	}
 
 	@PostMapping("/myCustomerDetails")
+	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod= "myCustomerDetailsFallBack")
+	//@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
 	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
@@ -70,5 +76,25 @@ public class AccoutsController {
 
 		return customerDetails;
 
+	}
+	
+	private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		return customerDetails;
+
+	}
+	
+	@GetMapping("/sayHello")
+	@RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+	public String sayHello() {
+		return "Hello, Welcome to EazyBank";
+	}
+
+	private String sayHelloFallback(Throwable t) {
+		return "Hi, Welcome to EazyBank";
 	}
 }
