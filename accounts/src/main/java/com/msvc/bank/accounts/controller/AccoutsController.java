bd6d54ce62d09361b2658813db556cef60ae068d
drondon87 <domingorondon87@gmail.com>
2023-06-28 10:45:09 +0200

    Seccion 8 se agrega el eureka server

--- accounts/src/main/java/com/msvc/bank/accounts/controller/AccoutsController.java
+++ accounts/src/main/java/com/msvc/bank/accounts/controller/AccoutsController.java
@@ -1,5 +1,7 @@
 package com.msvc.bank.accounts.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.msvc.bank.accounts.config.AccountsServiceConfig;
 import com.msvc.bank.accounts.model.Accounts;
+import com.msvc.bank.accounts.model.Cards;
 import com.msvc.bank.accounts.model.Customer;
+import com.msvc.bank.accounts.model.CustomerDetails;
+import com.msvc.bank.accounts.model.Loans;
 import com.msvc.bank.accounts.model.Properties;
 import com.msvc.bank.accounts.repository.AccountsRepository;
+import com.msvc.bank.accounts.service.client.CardsFeignClient;
+import com.msvc.bank.accounts.service.client.LoansFeignClient;
 
 @RestController
 public class AccoutsController {
@@ -23,6 +30,12 @@ public class AccoutsController {
 
 	@Autowired
 	private AccountsServiceConfig accountsConfig;
+
+	@Autowired
+	LoansFeignClient loansFeignClient;
+
+	@Autowired
+	CardsFeignClient cardsFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -42,5 +55,20 @@ public class AccoutsController {
 				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
+	}
+
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+
+		return customerDetails;
+
 	}
 }
